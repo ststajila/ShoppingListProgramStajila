837f132b38e9115c9523559333a41d018faf428f
Stanislav <stazilastas@gmail.com>
2023-11-01 09:49:40 -0500

    Basic table view to display items & add functionality

--- ShoppingListProgramStajila/ViewController.swift
+++ ShoppingListProgramStajila/ViewController.swift
@@ -7,13 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var mainTableView: UITableView!
+    
+    
+    var items: [String] = ["Milk", "Bread", "Eggs"]
+    
+    @IBOutlet weak var itemOutlet: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        mainTableView.delegate = self
+        mainTableView.dataSource = self
+        
     }
-
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = mainTableView.dequeueReusableCell(withIdentifier: "itemsCell")
+        cell!.textLabel?.text = items[indexPath.row]
+        return cell!
+    }
+    
+    @IBAction func add(_ sender: Any) {
+        
+        if itemOutlet.text != "" {
+            items.append(itemOutlet.text!)
+            mainTableView.reloadData()
+            itemOutlet.text = ""
+        } else{
+            
+        }
+        
+    }
+    
 
 }
 
